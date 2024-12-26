c65953402792efc3fb03a6881b61b0e1195a4387
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-12-26 20:59:09 +0530

    Headers component done

---
+++ app/ui/Header.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Header = () => {
+    const getCurrentDate = () => {
+        const today = new Date();
+        const day = String(today.getDate()).padStart(2, '0'); // Get day and pad with zero if needed
+        const month = String(today.getMonth() + 1).padStart(2, '0'); // Get month (0-indexed) and pad
+        const year = today.getFullYear(); // Get full year
+        return `${day}/${month}/${year}`; // Format as DD/MM/YYYY
+    }
+
+    return (
+        <div className='text-black flex flex-col h-full'>
+            <div className='h-1/2 flex border-2 border-red-500'>
+                <div className='relative'>
+                    <h1 className='font-[Sinosans] text-xl pt-4'>Date: </h1>
+                    <span className='h-[2px] w-full bg-black absolute bottom-0'></span>
+                </div>
+                <div className='relative'>
+                    <h1 className='font-[Signature] pt-4 px-12'>{getCurrentDate()}</h1>
+                    <span className='h-[2px] w-full bg-black absolute bottom-0'></span>
+                </div>
+            </div>
+            <div className='h-1/2 flex border-2 border-red-500'>
+                <div className='relative'>
+                    <h1 className='font-[Sinosans] text-xl pt-4'>Project: </h1>
+                    <span className='h-[2px] w-full bg-black absolute bottom-0'></span>
+                </div>
+                <div className='relative'>
+                    <h1 className='font-[Signature] pt-4 px-12'>Portfolio</h1>
+                    <span className='h-[2px] w-full bg-black absolute bottom-0'></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
