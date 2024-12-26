ad5e770a3a282406087deed3326f4cb70ea00174
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-12-26 21:17:08 +0530

    dottedgrid component try

--- app/ui/DottedBg.tsx
+++ app/ui/DottedBg.tsx
@@ -1,11 +1,11 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from "react";
 
-const DottedBg = ({ children }: { children: ReactNode }) => {
+function DottedBg({ children }: { children: ReactNode }) {
     return (
-        <div className="h-full w-full dark:bg-black bg-white dark:bg-dot-white bg-dot-black absolute -z-10 flex items-center justify-center">
+        <div className="h-full z-10 w-full bg-dot-black relative flex items-center justify-center">
             {children}
         </div>
-    )
+    );
 }
 
 export default DottedBg
