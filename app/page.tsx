1180b44d6e72146d4539fb221df2df316d890777
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-12-26 19:55:23 +0530

    NEXTJS IS SHIT

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Homepage from "@/components/Homepage";
-
-export default function Home() {
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <Homepage />
-    </div >
-  );
-}
