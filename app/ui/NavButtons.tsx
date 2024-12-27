f4a029264eeb144ad83d057a68f28fe52123f0c3
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-12-28 02:46:01 +0530

    navbuttons placeholder

---
+++ app/ui/NavButtons.tsx
@@ -0,0 +1,11 @@
+const NavButtons = () => {
+    return (
+        <div className="w-full h-full pt-6 flex justify-around items-center">
+            <button className='bg-[#ea93d3]/[0.5] h-12 w-12 rounded-lg'>hi</button>
+            <button className='bg-[#af89f2]/[0.5] h-12 w-12 rounded-lg'>hi</button>
+            <button className='bg-[#689f63]/[0.5] h-12 w-12 rounded-lg'>hi</button>
+        </div>
+    )
+}
+
+export default NavButtons
