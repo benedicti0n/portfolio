40b119f3b64d499716b8f6d9f293c493961528a9
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-12-27 17:41:11 +0530

    minor changein padding and add children

--- app/ui/DottedBg.tsx
+++ app/ui/DottedBg.tsx
@@ -1,7 +1,10 @@
-function DottedBg() {
+import { ReactNode } from "react";
+
+function DottedBg({ children }: { children: ReactNode }) {
     return (
         <div className="h-full w-full pt-6 pb-2">
-            <div className="h-full w-full z-10 bg-dot-[#BBBBBB] relative flex items-center justify-center">
+            <div className="h-full w-full z-10 bg-dot-[#BBBBBB] relative p-4">
+                {children}
             </div>
         </div>
     );
