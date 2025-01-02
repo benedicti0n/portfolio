881a6efeb52b239ee72580a63e9b13a4ac3d4925
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-02 15:58:48 +0530

    intro component

--- app/components/AboutMe/Intro.tsx
+++ app/components/AboutMe/Intro.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import Heading from "../Heading"
 
 const Intro = () => {
     return (
-        <div>Intro</div>
+        <div className='col-span-2 border-2 border-blue-500'>
+            <Heading heading="I <3 building products people love" icon="" color="#ea93d3" />
+            <h1 className="font-[Syne] text-black pt-2 pl-4">Full Stack Developer with a knack for transforming ideas into engaging digital experiences. I thrive on building innovative, user-focused products that bridge creativity and technology. Let’s create something extraordinary together!</h1>
+        </div>
     )
 }
 
