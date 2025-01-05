4b24468360cec197ebf1b16a661c26bc76788ec9
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-05 20:47:59 +0530

    new grid layout

--- app/components/AboutMe/AboutMyself.tsx
+++ app/components/AboutMe/AboutMyself.tsx
@@ -1,17 +1,35 @@
-// import Intro from "./Intro"
+import Intro from "./Intro"
 // import Song from "./Song"
 import { GithubGraph } from "./Github"
 
 const AboutMyself = () => {
     return (
-        <div className='grid grid-cols-3 gap-4'>
-            {/* <Intro />
-            <Song /> */}
-            <GithubGraph
-                username="benedicti0n"
-                blockMargin={2}
-                colorPallete={["#ffe5ec", "#ffc2d1", "#ffb3c6", "#ff8fab", "#fb6f92"]}
-            />
+        <div className='w-full grid grid-cols-3 gap-4'>
+            <div className=" col-span-2">
+                <Intro />
+
+            </div>
+            <div className="col-span-1">
+                {/* <Song /> */}
+                hi
+            </div>
+
+            <div className=" col-span-2">
+                <div className="w-full border-2 border-blue-500 text-black font-[Syne]">
+                    <GithubGraph
+                        username="benedicti0n"
+                        blockMargin={2}
+                        colorPallete={["#ffe5ec", "#ffc2d1", "#ffb3c6", "#ff8fab", "#fb6f92"]}
+                    />
+                </div>
+            </div>
+            <div className="col-span-1">
+
+            </div>
+
+            <div className=" col-span-3">
+                hi
+            </div>
         </div>
     )
 }
