b231dfae00d8ebcad8f8714a29255eaf28e60c53
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-07 18:45:28 +0530

    song component fully done but not styling

--- app/components/AboutMe/Song.tsx
+++ app/components/AboutMe/Song.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect, useRef } from "react";
 
 import "./slider.css"
-import { LuHeart } from "react-icons/lu";
+import { LuHeart, LuPlay, LuPause } from "react-icons/lu";
+
 
 const song = "/song/DieWithASmile.mp3"
 
@@ -9,6 +10,7 @@ const Song = () => {
     const [audioMetadata, setAudioMetadata] = useState<{ duration: number | null }>({ duration: null })
     const [currentTime, setCurrentTime] = useState(0);
     const audioRef = useRef<HTMLAudioElement | null>(null);
+    const [isPlaying, setIsPlaying] = useState<boolean>(false)
 
     useEffect(() => {
         audioRef.current = new Audio(song);
@@ -25,6 +27,8 @@ const Song = () => {
             setCurrentTime(audio.currentTime);
         }
 
+        setIsPlaying
+
         return () => {
             audio.pause();
         }
@@ -32,11 +36,25 @@ const Song = () => {
 
     const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newTime = parseFloat(event.target.value);
+
         setCurrentTime(newTime);
         if (audioRef.current) {
             audioRef.current.currentTime = newTime;
             console.log(currentTime);
 
+        }
+    }
+
+    const togglePlay = () => {
+        if (audioRef.current) {
+            if (audioRef.current.paused) {
+                audioRef.current.play();
+                setIsPlaying(true);
+            } else {
+                audioRef.current.pause();
+                setIsPlaying(false);
+            }
+            setCurrentTime(Math.floor(audioRef.current.currentTime));
         }
     }
 
@@ -48,15 +66,15 @@ const Song = () => {
                 <LuHeart fill="red" stroke="black" />
             </div>
 
-            <div className="h-full w-full border-2 border-blue-500">
+            <div className="h-full w-full ml-2 border-2 border-blue-500">
 
                 {/* name and artist name */}
-                <div className="text-black">
-                    <h1>Die with a smile</h1>
-                    <h3>Lady Gaga & Bruno Mars</h3>
+                <div className="text-black border-2 border-orange-500">
+                    <h1 className="text-2xl font-[Sinosans]">Die with a smile</h1>
+                    <h3 className="text-xs font-[Sinosans]">Lady Gaga • Bruno Mars</h3>
                 </div>
 
-                <div>
+                <div className="w-full border-2 border-cyan-500">
                     {/* Slider for audio control */}
                     <input
                         type="range"
@@ -64,25 +82,29 @@ const Song = () => {
                         max={audioMetadata.duration || 0}
                         value={currentTime}
                         onChange={handleSliderChange}
-                        className="slider"
+                        className="slider custom-slider"
                     />
                 </div>
 
                 {/* start time and end time */}
-                <div className="text-black flex justify-between">
-                    <h1>{Math.floor(currentTime / 60)}:{currentTime % 60}</h1>
-                    <h1>{Math.floor(audioMetadata.duration! / 60)}:{Math.floor(audioMetadata.duration!) % 60}</h1>
+                <div className="text-black flex justify-between border-2 border-orange-500">
+                    <h1 className="text-sm font-[Sinosans]">{Math.floor(currentTime / 60)}:{Math.floor(currentTime % 60)}</h1>
+                    <h1 className="text-sm font-[Sinosans]">{Math.floor(audioMetadata.duration! / 60)}:{Math.floor(audioMetadata.duration!) % 60}</h1>
                 </div>
 
                 {/* controlls */}
-                <div className="flex items-center justify-center mt-4">
-                    <audio controls >
+                <div className="flex items-center justify-center">
+                    <audio ref={audioRef} className="audio-player" autoPlay>
                         <source src={song} type="audio/mp3" />
-                        {/* <track kind="captions" srcLang="en" src="" default /> */}
+                        <track kind="captions" srcLang="en" src="" default />
                     </audio>
+                    {isPlaying ? (
+                        <LuPause onClick={togglePlay} fill="red" stroke="black" />
+                    ) : (
+                        <LuPlay onClick={togglePlay} fill="red" stroke="black" />
+                    )}
                 </div>
             </div>
-
 
         </div>
     )
