4f9c4aa2db1536f034d769ab34690fe0c734d46a
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-10 21:31:30 +0530

    github component mostly done

--- app/components/AboutMe/Github.tsx
+++ app/components/AboutMe/Github.tsx
@@ -1,9 +1,12 @@
 "use client";
 import React, { useCallback, useEffect, useState } from "react";
 
+import Heading from "../Heading";
+import FollowButton from "~/ui/FollowButton";
 import { Activity, ActivityCalendar } from "react-activity-calendar";
 import { Tooltip as ReactTooltip } from "react-tooltip";
 // import "react-tooltip/dist/react-tooltip.css"
+
 
 type GithubGraphProps = {
   username: string;
@@ -17,6 +20,7 @@ export const GithubGraph = ({
   colorPallete,
 }: GithubGraphProps) => {
   const [contribution, setContribution] = useState<Activity[]>([]);
+  const [githubData, setGithubData] = useState<githubDataProps>()
   const [loading, setIsLoading] = useState<boolean>(true);
 
   const fetchData = useCallback(async () => {
@@ -30,8 +34,18 @@ export const GithubGraph = ({
     }
   }, [username]);
 
+  const fetchGithubDetails = useCallback(async () => {
+    try {
+      const githubData = await fetchGithubData(username)
+      setGithubData(githubData)
+    } catch (error) {
+      throw Error("Error fetching github data");
+    }
+  }, [username])
+
   useEffect(() => {
     fetchData();
+    fetchGithubDetails();
   }, [fetchData]);
 
   const label = {
@@ -39,7 +53,20 @@ export const GithubGraph = ({
   };
 
   return (
-    <>
+    <div className="w-full border-2 border-blue-500">
+      <Heading heading="Github" icon="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_960_720.png" color="#000" />
+
+      <div className="flex">
+        <img src={githubData?.avatarUrl} alt={`${username}_avatar`} className="h-24" />
+
+        <div>
+          <h1>@{username}</h1>
+          <h1>{githubData?.bio}</h1>
+          <h1>Followers: {githubData?.followers}</h1>
+          <FollowButton color="black" />
+        </div>
+      </div>
+
       <ActivityCalendar
         data={contribution}
         maxLevel={4}
@@ -65,9 +92,10 @@ export const GithubGraph = ({
         }}
       />
       <ReactTooltip id="react-tooltip" />
-    </>
+    </div>
   );
 };
+
 async function fetchContributionData(username: string): Promise<Activity[]> {
   const response = await fetch(`https://github.vineet.tech/api/${username}`);
   const responseBody = await response.json();
@@ -76,4 +104,29 @@ async function fetchContributionData(username: string): Promise<Activity[]> {
     throw Error("Erroring fetching contribution data");
   }
   return responseBody.data;
+}
+
+interface githubDataProps {
+  username: string;
+  avatarUrl: string;
+  bio: string;
+  followers: number;
+}
+
+async function fetchGithubData(username: string): Promise<githubDataProps> {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const data = await response.json()
+
+  if (!response.ok) {
+    throw Error("Error fetching github data")
+  }
+
+  const githubData: githubDataProps = {
+    username: username,
+    avatarUrl: data.avatar_url,
+    bio: data.bio,
+    followers: data.followers
+  }
+
+  return githubData;
 }
