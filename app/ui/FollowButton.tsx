f58ded62537df8ed87ee8876f784069da61e1a49
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-14 09:41:29 +0530

    new follow button

--- app/ui/FollowButton.tsx
+++ app/ui/FollowButton.tsx
@@ -1,6 +1,17 @@
-const FollowButton = ({ color }: { color: string }) => {
+interface IFollowButtonProps {
+    color: string;
+    text: string;
+    textColor: string;
+    children: React.ReactNode;
+    iconColor: string;
+}
+const FollowButton = ({ color, text, textColor, children, iconColor }: IFollowButtonProps) => {
     return (
-        <button className={`bg-${color}/50 px-6 py-1 rounded-xl`}>Follow</button>
+        <div className='mt-6 w-24 flex text-center relative font-[Sinosans]'>
+            <button className={`w-full px-4 py-2 rounded-lg border-[1px] border-black top-0 left-0 hover:top-[1px] hover:left-[1px] transition-all duration-300 relative flex justify-center items-center`} style={{ backgroundColor: `${color}`, color: `${textColor}` }}>{text}<span className={`ml-2 ${iconColor}`}>{children}</span>
+            </button>
+            <span className='w-full h-full absolute top-1 left-1 -z-10 bg-black rounded-lg'></span>
+        </div>
     )
 }
 
