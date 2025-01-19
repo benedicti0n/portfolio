1621fd06d14c65a719d89600a2f3fc800ac76053
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2025-01-19 08:56:47 +0530

    yt data for testing now

---
+++ app/youtube.ts
@@ -0,0 +1,103 @@
+export const youtubeData = {
+  "kind": "youtube#channelListResponse",
+  "etag": "rjmS0Y1OfCarHOV-QAN4ZHDG-MU",
+  "pageInfo": {
+    "totalResults": 1,
+    "resultsPerPage": 5
+  },
+  "items": [
+    {
+      "kind": "youtube#channel",
+      "etag": "bT__YrgbuNj5ZL2fe-zeiTdzWcg",
+      "id": "UCBzgyH8XwgPKryu0ycld-Fg",
+      "snippet": {
+        "title": "Benediction",
+        "description": "",
+        "customUrl": "@benedictionash03",
+        "publishedAt": "2024-10-31T08:01:40.281397Z",
+        "thumbnails": {
+          "default": {
+            "url": "https://yt3.ggpht.com/_fGRGW4l86yizSmDdSc9iPAKt0FmLdS0ioIEmYQ_-TL232-G9uTdngSpyN3Kl0vjjvBa8iosWUo=s88-c-k-c0x00ffffff-no-rj",
+            "width": 88,
+            "height": 88
+          },
+          "medium": {
+            "url": "https://yt3.ggpht.com/_fGRGW4l86yizSmDdSc9iPAKt0FmLdS0ioIEmYQ_-TL232-G9uTdngSpyN3Kl0vjjvBa8iosWUo=s240-c-k-c0x00ffffff-no-rj",
+            "width": 240,
+            "height": 240
+          },
+          "high": {
+            "url": "https://yt3.ggpht.com/_fGRGW4l86yizSmDdSc9iPAKt0FmLdS0ioIEmYQ_-TL232-G9uTdngSpyN3Kl0vjjvBa8iosWUo=s800-c-k-c0x00ffffff-no-rj",
+            "width": 800,
+            "height": 800
+          }
+        },
+        "localized": {
+          "title": "Benediction",
+          "description": ""
+        }
+      },
+      "statistics": {
+        "viewCount": "5100",
+        "subscriberCount": "78",
+        "hiddenSubscriberCount": false,
+        "videoCount": "78"
+      },
+      "brandingSettings": {
+        "channel": {
+          "title": "Benediction"
+        },
+        "image": {
+          "bannerExternalUrl": "https://yt3.googleusercontent.com/wrLtt-ApipOtK6G3r6cblbFpmDX-6OFpnPKGXz9JUY_bTFk5_jKgOMrHP-METxBxQaWiRcRO"
+        }
+      }
+    }
+  ]
+}
+
+export const videosData = {
+  "kind": "youtube#searchListResponse",
+  "etag": "QmiKgaN2MvrrAN0i7wlN1i3lqAw",
+  "nextPageToken": "CAEQAA",
+  "regionCode": "IN",
+  "pageInfo": {
+    "totalResults": 78,
+    "resultsPerPage": 1
+  },
+  "items": [
+    {
+      "kind": "youtube#searchResult",
+      "etag": "0ChyCA1-3R7xGVFCp1F5Hi15LlQ",
+      "id": {
+        "kind": "youtube#video",
+        "videoId": "2nCCOvUOlDQ"
+      },
+      "snippet": {
+        "publishedAt": "2025-01-15T15:54:13Z",
+        "channelId": "UCBzgyH8XwgPKryu0ycld-Fg",
+        "title": "Dev Log: Day 62 | Coding Everyday",
+        "description": "This stream is created with #PRISMLiveStudio.",
+        "thumbnails": {
+          "default": {
+            "url": "https://i.ytimg.com/vi/2nCCOvUOlDQ/default.jpg",
+            "width": 120,
+            "height": 90
+          },
+          "medium": {
+            "url": "https://i.ytimg.com/vi/2nCCOvUOlDQ/mqdefault.jpg",
+            "width": 320,
+            "height": 180
+          },
+          "high": {
+            "url": "https://i.ytimg.com/vi/2nCCOvUOlDQ/hqdefault.jpg",
+            "width": 480,
+            "height": 360
+          }
+        },
+        "channelTitle": "Benediction",
+        "liveBroadcastContent": "none",
+        "publishTime": "2025-01-15T15:54:13Z"
+      }
+    }
+  ]
+}
